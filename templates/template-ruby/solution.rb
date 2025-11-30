81644a5457dc238b4ddce8a042db79423769f328
Jon Lovering <jon.lovering@gmail.com>
2025-11-30 12:28:55 -0600

    Add Ruby Template from Neenjaw
    Original https://github.com/neenjaw/advent-of-code/tree/master/templates/ruby

---
+++ templates/template-ruby/solution.rb
@@ -0,0 +1,20 @@
+class Solution
+  attr_reader :input_data
+
+  def initialize(input_data)
+    @input_data = input_data
+    # Process the input here, e.g., convert to an array of lines or integers
+    @lines = input_data.split("\n")
+  end
+
+  # Standard AoC methods
+  def part1
+    # Your Part 1 logic here
+    @lines.size # Example: just return the number of lines
+  end
+
+  def part2
+    # Your Part 2 logic here
+    "Not implemented yet"
+  end
+end
