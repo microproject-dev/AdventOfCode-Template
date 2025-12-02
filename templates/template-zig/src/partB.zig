37a311bb3f81a47ed826be5cb5b377c3d945becf
Jon Lovering <jon.lovering@gmail.com>
2025-12-02 01:01:30 -0600

    Add proper file reading and basic line by line parser stub.

--- templates/template-zig/src/partB.zig
+++ templates/template-zig/src/partB.zig
@@ -2,25 +2,40 @@ const std = @import("std");
 const common = @import("common");
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"partB"});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const file = try std.fs.cwd().openFile("input/input.txt", .{});
+    defer file.close();
+    const input = try file.readToEndAlloc(allocator, std.math.maxInt(usize));
+
+    const out = try solve(allocator, input);
+    defer allocator.free(out);
+    std.debug.print("PartB: {s}\n", .{out});
+}
+
+pub fn solve(allocator: std.mem.Allocator, input: []const u8) ![]const u8 {
+    _ = input;
+    return std.fmt.allocPrint(allocator, "", .{});
 }
 
 test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    const allocator = std.testing.allocator;
+    const input =
+        \\L68
+        \\L30
+        \\R48
+        \\L5
+        \\R60
+        \\L55
+        \\L1
+        \\L99
+        \\R14
+        \\L82
+    ;
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    const out = try solve(allocator, input);
+    defer allocator.free(out);
+
+    try std.testing.expectEqualStrings("3", out);
 }
--- templates/template-zig/src/root.zig
+++ templates/template-zig/src/root.zig
@@ -1,15 +1,14 @@
 //! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
 
-pub fn parseInput() !void {
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
+pub fn parseInput(allocator: std.mem.Allocator, input: []const u8) !std.array_list.Aligned(i32, null) {
+    var output = try std.ArrayList(i32).initCapacity(allocator, input.len);
+    errdefer output.deinit(allocator);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var it = std.mem.splitScalar(u8, input, '\n');
+    while (it.next()) |t| {
+        _ = t;
+    }
 
-    try stdout.flush(); // Don't forget to flush!
+    return output;
 }
